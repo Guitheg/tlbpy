061982f2ed64aa877f76a0b677e8aedcad48c27a
Guillaume Gobin <guigobin@gmail.com>
2020-03-21 11:02:12 +0100

    path

--- tlbpy/path.py
+++ tlbpy/path.py
@@ -8,7 +8,7 @@ def get_file_ext(file):
     return file.split(".")[-1]
 
 def get_files(dirpath, ext = [], basename=False):
-    return [os.path.join(dirpath, file) if not basename else file for file in os.listdir(dirpath) 
+    return [os.path.join(os.path.basename(dirpath), file) if not basename else file for file in os.listdir(dirpath) 
         if get_file_ext(file) in ext or 
             ext == [] or 
             (ext == ["*"] and os.path.isfile(os.path.join(dirpath, file)))]
