b55ba383dd189bc1e003f86c2a3950ff2b9f3002
Guillaume Gobin <guigobin@gmail.com>
2020-09-10 11:40:33 +0200

    delete duration

--- tlbpy/__init__.py
+++ tlbpy/__init__.py
@@ -4,9 +4,8 @@
 """
     My personnal toolbox for Python
 """
-__version__="0.0.14"
+__version__="0.0.15"
 from tlbpy.cnf_parser import cnf_parser
-from tlbpy.duration import Duration
 from tlbpy.path import get_file_ext, get_files
 from tlbpy import rl_algos
 from tlbpy.rl_algos import rl_algo, spg
