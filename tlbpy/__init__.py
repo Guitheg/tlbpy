9c14343c77aad124fb867bfea8f24200af55d007
Guillaume Gobin <guigobin@gmail.com>
2020-09-10 11:38:17 +0200

    0.0.14 (rl_algos)

--- tlbpy/__init__.py
+++ tlbpy/__init__.py
@@ -4,9 +4,9 @@
 """
     My personnal toolbox for Python
 """
-__version__="0.0.13"
-from tlbpy.helloworld import helloworld
+__version__="0.0.14"
 from tlbpy.cnf_parser import cnf_parser
 from tlbpy.duration import Duration
-from tlbpy.talk import Talk
 from tlbpy.path import get_file_ext, get_files
+from tlbpy import rl_algos
+from tlbpy.rl_algos import rl_algo, spg
